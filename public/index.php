220095ae3a2658a09baf3482d348b83296556086
Lukas Schönbeck <lukas.schoenbeck@gmx.de>
2020-08-03 13:49:54 +0200

    add public folder

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<?php
+
+use SCHOENBECK\Database\DatabaseConfigurator;
+
+include_once "../vendor/autoload.php";
+
+use SCHOENBECK\Database\DatabaseAdapter;
+use SCHOENBECK\Database\DatabaseConfig;
+use SCHOENBECK\Database\DatabaseConnection;
+
+
+$config = new DatabaseConfig("database", "lamp", "lamp", 3306, "lamp", "mysql");
+
+$connection = new DatabaseConnection($config);
+
+//echo $connection->__toString() . "<br>";
+
+$adaptor = new DatabaseAdapter($connection);
+
+//echo $adaptor->selectFromTable("test");
+
+
+
+$dbConfig = new DatabaseConfigurator($adaptor);
+
+$result = $dbConfig->checkDatabaseConfigYamlFile("./database.yml");
+
+print_r($result);
